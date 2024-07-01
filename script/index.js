3ccd616c7cb0e62c97170101158a6893c185ecba
sela-xyz <betsela123@gmail.com>
2024-07-01 16:45:03 +0700

    release 1.1

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-const KEY = "diary";
+const KEY = 'diary';
 
 const setItem = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 const getItem = (key) =>
@@ -11,9 +11,9 @@ const addNewDiary = () => {
     }
 
     const diary = getItem(KEY);
-    const title = document.getElementById("title").value;
-    const content = document.getElementById("content").value;
-    const date = document.getElementById("date").value;
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+    const date = document.getElementById('date').value;
 
     diary.push({
       id: diary.length + 1,
@@ -30,17 +30,17 @@ const addNewDiary = () => {
 
 const updateDiary = (index) => {
   const diary = getItem(KEY);
-  document.getElementById("submit").style.display = "none";
-  document.getElementById("update").style.display = "block";
+  document.getElementById('submit').style.display = 'none';
+  document.getElementById('update').style.display = 'block';
 
-  document.getElementById("title").value = diary[index].title;
-  document.getElementById("content").value = diary[index].content;
-  document.getElementById("date").value = diary[index].date;
+  document.getElementById('title').value = diary[index].title;
+  document.getElementById('content').value = diary[index].content;
+  document.getElementById('date').value = diary[index].date;
 
-  document.getElementById("update").onclick = function () {
-    diary[index].title = document.getElementById("title").value;
-    diary[index].content = document.getElementById("content").value;
-    diary[index].date = document.getElementById("date").value;
+  document.getElementById('update').onclick = function () {
+    diary[index].title = document.getElementById('title').value;
+    diary[index].content = document.getElementById('content').value;
+    diary[index].date = document.getElementById('date').value;
 
     setItem(KEY, diary);
     displayAllDiary();
@@ -66,25 +66,26 @@ function displayAllDiary() {
     setItem(KEY, []);
   }
 
-  const diaryList = document.getElementById("diary-list");
-  const diaryItem = document.getElementById("diary-item");
+  const diaryList = document.getElementById('diary-list');
+  const diaryItem = document.getElementById('diary-item');
 
   const diary = getItem(KEY);
 
-  let html = "";
+
+  let html = '';
   diaryItem.innerHTML = `Found (${diary.length}) ${
-    diary.length <= 1 ? "diary" : "diaries"
+    diary.length <= 1 ? 'diary' : 'diaries'
   }`;
 
   diary.forEach((data, index) => {
-    const item = document.createElement("div");
-    item.classList.add("diary-card");
+    const item = document.createElement('div');
+    item.classList.add('diary-card');
     item.innerHTML = `
       <div class="content">
           <div class="summary">
             <h2>${stringLimiter(data.title, 20)}</h2>
             <p>${stringLimiter(data.content, 20)}</p>
-            <p>${data.date}</p>
+            <p>${dateFormat(data.date)}</p>
           </div>
         <div class="action">
           <div onclick="updateDiary(${index})"><img src="../assets/icons/edit-icon.svg"></div>
@@ -96,19 +97,31 @@ function displayAllDiary() {
       <div>
       <div popover id="${index}" class="diary-modal">
         <div class="diary-modal-header">
+              <div class="diary-modal-close"> 
+        <button popovertarget="${index}" popovertargetaction="hide">
+        <img src="../assets/icons/close.svg" >
+        </button> 
+      </div>
           <div>
            <h2>${data.title}</h2>
-           <h3>${data.date}</h3>
+           <p>${dateFormat(data.date)}</p>
         </div>
-        <button popovertarget="${index}" popovertargetaction="hide"><img src="../assets/icons/close.svg" ></button>
+     
         </div>
         <hr>
-        <p>${data.content}</p>
+        <p class='diary-modal-content'>${data.content}</p>
     </div>
         `;
     html += `${item.outerHTML}`;
   });
+
   diaryList.innerHTML = html;
+  if(diary.length === 0){
+    diaryList.innerHTML = `<div class='diary-list-empty'>
+      <img src="../../assets/images/empty-list.png">
+      <p>Your diary is empty, start writing some</p>
+    </div>`
+  }
 }
 
 function clearAll() {
@@ -117,44 +130,63 @@ function clearAll() {
 }
 
 function validate() {
-  const title = document.getElementById("title").value;
-  const content = document.getElementById("content").value;
-  const date = document.getElementById("date").value;
-  if (title === "") {
-    alert("Title is empty");
+  const title = document.getElementById('title').value;
+  const content = document.getElementById('content').value;
+  const date = document.getElementById('date').value;
+  if (title === '') {
+    alert('Title is empty');
     return false;
   }
-  if (content === "") {
-    alert("Content is empty");
+  if (content === '') {
+    alert('Content is empty');
     return false;
   }
-  if (date === "") {
-    alert("date is empty");
+  if (date === '') {
+    alert('date is empty');
     return false;
   }
   return true;
 }
 
 function displayManual() {
-  const manualPopover = document.querySelector(".manual-popover");
-  manualPopover.classList.toggle("manual-popover-display");
+  const manualPopover = document.querySelector('.manual-popover');
+  manualPopover.classList.toggle('manual-popover-display');
 }
 
 function resetForm() {
-  document.getElementById("title").value = "";
-  document.getElementById("content").value = "";
-  document.getElementById("date").value = "";
-  document.getElementById("submit").style.display = "block";
-  document.getElementById("update").style.display = "none";
+  document.getElementById('title').value = '';
+  document.getElementById('content').value = '';
+  document.getElementById('date').value = '';
+  document.getElementById('submit').style.display = 'block';
+  document.getElementById('update').style.display = 'none';
 }
 
 function stringLimiter(string, limit) {
-  let dots = "...";
+  let dots = '...';
   if (string.length > limit) {
     string = string.substring(0, limit) + dots;
   }
 
   return string;
+}
+
+function dateFormat(inputDate) {
+  // Create a new Date object
+  let date = new Date(inputDate);
+
+  // Options for formatting the date
+  let options = {
+    weekday: 'short',
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+  };
+
+  // Format the date
+  let formattedDate = date.toLocaleDateString('en-US', options);
+
+  // Print the formatted date
+  return formattedDate;
 }
 
 document.onload = displayAllDiary();
